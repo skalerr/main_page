c789ec3d722a983e39f8b5c9f9a09b01f0a2cc5f
Robert <rob06121@gmail.com>
2024-04-27 23:53:33 +0500

    change to stateless

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:main_page/widgets/cards.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
