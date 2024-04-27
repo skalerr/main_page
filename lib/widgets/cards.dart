deebf12b8da3139082aa854b487ed3a86a28d9ac
Robert <rob06121@gmail.com>
2024-04-27 23:50:32 +0500

    add controller

--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -3,20 +3,26 @@ import 'package:flutter/material.dart';
 class CardGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: _calculateCrossAxisCount(context),
-      ),
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: EdgeInsets.all(16.0),
-          child: CardItem(
-            title: 'Карточка ${index + 1}',
-            subtitle: 'Описание карточки ${index + 1}',
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Align(
+        alignment: Alignment.center,
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: _calculateCrossAxisCount(context),
           ),
-        );
-      },
-      itemCount: 9,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.all(16.0),
+              child: CardItem(
+                title: 'Карточка ${index + 1}',
+                subtitle: 'Описание карточки ${index + 1}',
+              ),
+            );
+          },
+          itemCount: 9,
+        ),
+      ),
     );
   }
 
@@ -47,23 +53,21 @@ class CardItem extends StatelessWidget {
         height: 120.0,
         padding: EdgeInsets.all(12.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment
-              .center, // Выравниваем содержимое по центру по вертикали
-          crossAxisAlignment: CrossAxisAlignment
-              .center, // Выравниваем содержимое по центру по горизонтали
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Icon(Icons.card_giftcard, size: 32.0),
             SizedBox(height: 8.0),
             Text(
               title,
               style: TextStyle(fontSize: 16.0),
-              textAlign: TextAlign.center, // Выравниваем текст по центру
+              textAlign: TextAlign.center,
             ),
             SizedBox(height: 4.0),
             Text(
               subtitle,
               style: TextStyle(fontSize: 14.0),
-              textAlign: TextAlign.center, // Выравниваем текст по центру
+              textAlign: TextAlign.center,
             ),
           ],
         ),
